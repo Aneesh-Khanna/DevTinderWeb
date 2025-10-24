6d4acf4b9bfceab76316c732db1831fd055c7a49
Aneesh-Khanna <akkakk1973@gmail.com>
2025-10-24 07:05:19 +0530

    basic routing done && basic redux store added && custom offline page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import Header from "./components/Header"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import appStore from "./utils/ReduxStore/appStore";
+import { Provider } from "react-redux";
+import useOnlineStatus from './customHooks/useOnlineStatus';
+import CustomOfflinePage from './components/CustomOfflinePage';
+
+import Body from "./components/Body";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
 function App() {
+
+  const isOnline = useOnlineStatus();
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body/>,
+      children: [
+        {
+          path: "/",
+          element: <Feed/>,
+        },
+        {
+          path: "/profile",
+          element: <Profile/>,
+        },
+        {
+          path: "/login",
+          element: <Login/>,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
-      <div> 
-        <Header/>
-      </div>
+      <Provider store={appStore}>
+      {/* Conditional render based on online status */}
+      {!isOnline ? (
+        <CustomOfflinePage />
+      ) : (
+        <RouterProvider router={appRouter} />
+      )}
+      </Provider>
     </>
   )
 }
