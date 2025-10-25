ed199a2dde41ee54cab924532ba715ce8cdc1342
Aneesh-Khanna <akkakk1973@gmail.com>
2025-10-26 02:03:05 +0530

    created env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:3001"
+    : import.meta.env.VITE_API_URL;
 
 export const LOGO =
   "https://static.whatsapp.net/rsrc.php/v4/yc/r/hUUuVTz6ZVi.png";
